7bd092a9e3ca1307ba705ed7320cb70dca8991ac
ashraf56 <ashrafulfahim07@gmail.com>
2024-02-19 08:12:47 +0600

    Image size fixed

--- src/app/component/Chats/Chats.jsx
+++ src/app/component/Chats/Chats.jsx
@@ -6,14 +6,14 @@ const Chats = ({ msg }) => {
         <div className='w-full items-center justify-center  '>
             <div className={` card w-full py-4 px-16 md:px-24  lg:px-52 `}>
                 <div className='flex '>
-                    <div className='font-bold px-3  rounded-full'>{msg.role === 'user' ?
+                    <div className='font-bold px-3  rounded-full '>{msg.role === 'user' ?
                         <div className="avatar placeholder">
                             <div className="bg-neutral text-neutral-content rounded-full w-8">
                                 <span className="text-neutral-content">A</span>
                             </div>
                         </div>
                         :
-                        <Image src={imf} width={50} height={50} alt='i' className='rounded-full w-8'  />}</div>
+                        <div className='rounded-full w-8'><Image src={imf} width={50} height={50} alt='i'   /></div> }</div>
                     <p>{msg.content}</p>
                 </div>
             </div>
